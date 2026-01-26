0ba827ea1922da2562d6e40662f11294bfc05bf9
krisb <anandhukrishna10b@gmail.com>
2026-01-27 01:52:29 +0530

    first completion

--- navbar.js
+++ navbar.js
@@ -518,6 +518,7 @@ class FloatingNavbar extends HTMLElement {
                 <!-- Visible Links (Priority+) -->
                 <ul class="navbar-links" id="navbar-links">
                     <li><a href="index.html">2027 ICSWHMH</a></li>
+                    <li><a href="history.html">History</a></li>
                     <li>
                         <a href="program.html">Program</a>
                         <ul class="dropdown-menu">
